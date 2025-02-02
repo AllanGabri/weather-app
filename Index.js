acba778eee48b42f9ff43e311c51c233b177b1d6
Allan Gabriel <100225161+AllanGabri@users.noreply.github.com>
2025-02-01 21:54:04 -0500

    Index.js

---
+++ Index.js
@@ -0,0 +1,50 @@
+/* WeatherApp.css */
+
+body {
+  font-family: Arial, sans-serif;
+  background-image: url('/home/user/weatherapp/weather-app/src/weather-app_img.jpg');
+  background-size: cover; 
+  margin-top: 120px;
+  padding: 20px;
+  
+}
+
+h1 {
+  text-align: center;
+  color: #333; /* Darker text for better readability */
+}
+
+h2 {
+  text-align: center;
+  color: #0d4988; /* Bootstrap primary color */
+}
+
+div {
+  border-radius: 8px;
+  padding: 20px;
+  background-color: rgb(239, 252, 253); /* White background for content */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Subtle shadow */
+  max-width: 400px;
+  margin: auto; /* Center the content */
+}
+
+p {
+  font-size: 16px;
+  color: #555; /* Slightly lighter text for paragraphs */
+  text-align: center;
+}
+
+.error {
+  color: red; /* Red color for error messages */
+  text-align: center;
+}
+
+.loading {
+  text-align: center;
+}
+
+button{
+  text-align: center;
+  font-size: 16px;
+  background-color: #e6e6e6;
+}
